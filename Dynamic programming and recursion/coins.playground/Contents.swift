285ea90c4a55643bf7fae08a45e1b5330e5c02af
Nik Flahavan <nflahavan@gmail.com>
2020-06-20 13:28:44 -0700

    coins: Solution, no memoization.

---
+++ Dynamic programming and recursion/coins.playground/Contents.swift
@@ -0,0 +1,89 @@
+func changePossibilities(amount: Int, denominations: [Int]) -> Int {
+  guard amount > 0 else { return 1 }
+  guard !denominations.isEmpty else { return 0 }
+  
+  let sortedDenominations = denominations.sorted()
+  let lowestDenomination = sortedDenominations.first!
+  let otherDenominations = Array(denominations[1..<denominations.endIndex])
+  var numLowestDenom = amount / lowestDenomination
+  var poss = 0
+  
+  while numLowestDenom >= 0 {
+    let remainder = amount - numLowestDenom * lowestDenomination
+    poss += changePossibilities(amount: remainder, denominations: otherDenominations)
+    numLowestDenom -= 1
+  }
+  
+  return poss
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// tests
+
+import XCTest
+
+class Tests: XCTestCase {
+  
+  func testSampleInput() {
+    let actual = changePossibilities(amount: 4, denominations: [1, 2, 3])
+    let expected = 4
+    XCTAssertEqual(actual, expected)
+  }
+  
+  func testOneWayToMakeZeroCents() {
+    let actual = changePossibilities(amount: 0, denominations: [1, 2])
+    let expected = 1
+    XCTAssertEqual(actual, expected)
+  }
+  
+  func testNoWaysIfNoCoins() {
+    let actual = changePossibilities(amount: 1, denominations: [])
+    let expected = 0
+    XCTAssertEqual(actual, expected)
+  }
+  
+  func testBigCoinValue() {
+    let actual = changePossibilities(amount: 5, denominations: [25, 50])
+    let expected = 0
+    XCTAssertEqual(actual, expected)
+  }
+  
+  func testBigTargetAmount() {
+    let actual = changePossibilities(amount: 50, denominations: [5, 10])
+    let expected = 6
+    XCTAssertEqual(actual, expected)
+  }
+  
+  func testChangeForOneDollar() {
+    let actual = changePossibilities(amount: 100, denominations: [1, 5, 10, 25, 50])
+    let expected = 292
+    XCTAssertEqual(actual, expected)
+  }
+  
+  static let allTests = [
+    ("testSampleInput", testSampleInput),
+    ("testOneWayToMakeZeroCents", testOneWayToMakeZeroCents),
+    ("testNoWaysIfNoCoins", testNoWaysIfNoCoins),
+    ("testBigCoinValue", testBigCoinValue),
+    ("testBigTargetAmount", testBigTargetAmount),
+    ("testChangeForOneDollar", testChangeForOneDollar)
+  ]
+}
+
+Tests.defaultTestSuite.run()
