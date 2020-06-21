8f9777f4dd2310833a4784b665d1c318d4e1a689
Nik Flahavan <nflahavan@gmail.com>
2020-06-21 11:22:10 -0700

    random-practice: generating random number in range 0..<URLS.count

--- random-practice.sh
+++ random-practice.sh
@@ -1,3 +1,3 @@
 URLS=(https://www.google.com https://www.espn.com https://www.interviewcake.com https://www.npsfoothealth.com)
-NUM=$(($RANDOM%4))
+NUM=$(($RANDOM%${#URLS[@]}))
 open -a "Google Chrome" ${URLS[$NUM]}
