1a069c54157375d2d20da59fcb4a6de7010ec897
Nik Flahavan <nflahavan@gmail.com>
2020-06-21 10:09:35 -0700

    coins: provided memoized solution, dynamic solution.

--- Dynamic programming and recursion/coins.playground/Contents.swift
+++ Dynamic programming and recursion/coins.playground/Contents.swift
@@ -1,4 +1,20 @@
-func changePossibilities(amount: Int, denominations: [Int]) -> Int {
+func changePossibilitiesDynamic(amount: Int, denominations: [Int]) -> Int {
+  var waysOfDoing = Array(repeating: 0, count: amount + 1)
+  waysOfDoing[0] = 1
+  
+  for denomination in denominations {
+    if denomination <= amount {
+      for higherAmount in denomination...amount {
+        let higherAmountRemainder = higherAmount - denomination
+        waysOfDoing[higherAmount] += waysOfDoing[higherAmountRemainder]
+      }
+    }
+  }
+  
+  return waysOfDoing[amount]
+}
+
+func changePossibilitiesRecursiveMemoized(amount: Int, denominations: [Int]) -> Int {
   var memo = [Int:[[Int]:Int]]()
   
   func memoChangePoss(_ amount: Int, _ denominations: [Int]) -> Int {
@@ -31,8 +47,64 @@ func changePossibilities(amount: Int, denominations: [Int]) -> Int {
   return memoChangePoss(amount, denominations)
 }
 
+class ProvidedRecursiveMemoized {
+  
+  private var memo: [String: Int] = [:]
+  private var denominations: [Int] = []
+  
+  func changePossibilitiesTopDown(amountLeft: Int, denominations: [Int]) -> Int {
+    return changePossibilitiesTopDown(amountLeft: amountLeft, denominations: denominations, currentIndex: 0)
+  }
+  
+  func changePossibilitiesTopDown(amountLeft: Int, denominations: [Int], currentIndex: Int) -> Int {
+    
+    // check our memo and short-circuit if we've already solved this one
+    let memoKey = "\(amountLeft), \(currentIndex)"
+    if let memoValue = memo[memoKey] {
+      return memoValue
+    }
+    
+    // base cases:
+    // we hit the amount spot on. yes!
+    if amountLeft == 0 {
+      return 1
+    }
+    
+    // we overshot the amount left (used too many coins)
+    if amountLeft < 0 {
+      return 0
+    }
+    
+    // we're out of denominations
+    if currentIndex == denominations.count {
+      return 0
+    }
+        
+    // choose a current coin
+    let currentCoin = denominations[currentIndex]
+    
+    // see how many possibilities we can get
+    // for each number of times to use currentCoin
+    var numPossibilities = 0
+    var amountLeftInner = amountLeft
+    while amountLeftInner >= 0  {
+      numPossibilities += changePossibilitiesTopDown(amountLeft: amountLeftInner, denominations: denominations,
+                                                     currentIndex: currentIndex + 1)
+      amountLeftInner -= currentCoin
+    }
+    
+    // save the answer in our memo so we don't compute it again
+    memo[memoKey] = numPossibilities
+    
+    return numPossibilities
+  }
+}
 
-
+func changePossibilities(amount: Int, denominations: [Int]) -> Int {
+  //changePossibilitiesRecursiveMemoized(amount: amount, denominations: denominations)
+  changePossibilitiesDynamic(amount: amount, denominations: denominations)
+  //ProvidedRecursiveMemoized().changePossibilitiesTopDown(amountLeft: amount, denominations: denominations)
+}
 
 
 
